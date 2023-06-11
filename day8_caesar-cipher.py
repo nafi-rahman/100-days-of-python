4240c17efdf4edcf2ebd3b9db7b170073f64af94
nafi-rahman <nafirahman16@gmail.com>
2023-06-11 09:35:34 +0600

    Create day8_caesar-cipher.py

---
+++ day8_caesar-cipher.py
@@ -0,0 +1,20 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+shift = shift % 26
+
+def cypher(text_,shift_,direction_):
+    cipher = ""
+    if direction_.lower() == "decode":
+        shift_ *= -1    
+    for letter in text_:
+        if letter in alphabet:
+            position = alphabet.index(letter)
+            new_position = position + shift_
+            cipher += alphabet[new_position]
+        else: cipher += letter
+    print(cipher)
+
+cypher(text,shift,direction)
